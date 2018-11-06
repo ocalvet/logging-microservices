789b27f5b8169725848ccc5cf49bbb7b2389d62c
Ovidio R. Calvet <calveto@gmail.com>
2018-11-05 21:27:25 -0500

    Add admin service

---
+++ admin/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleHome)
+	log.Fatal(http.ListenAndServe(":8280", mux))
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "{ 'succes': 'true', 'service': 'admin' }")
+}
