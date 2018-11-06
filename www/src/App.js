71dcecac6c200e8e9ce3977b3270f32a18529e61
Ovidio R. Calvet <calveto@gmail.com>
2018-11-06 12:34:01 -0500

    Add reset css

--- www/src/App.js
+++ www/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Logging testing application</h1>
+        <div>Links</div>
+        <ul>
+          <li><a className="link" href="/users">Users</a></li>
+          <li><a className="link" href="/todos">Todos</a></li>
+          <li><a className="link" href="/admin">Admin</a></li>
+        </ul>
       </div>
     );
   }
